187140086a455155b25ddc9c3a016908b5c6b7d0
digitalspike <digitalspikeapps@gmail.com>
2023-10-16 21:44:17 +0530

    Display wishlist

--- lib/home_screens/wishlist_screen.dart
+++ lib/home_screens/wishlist_screen.dart
@@ -1,9 +1,15 @@
+import 'dart:convert';
+
 import 'package:ezys/custom_widgets/constants.dart';
 import 'package:ezys/custom_widgets/multiselectchoicechip_widget.dart';
 import 'package:ezys/custom_widgets/persistentheader.dart';
 import 'package:ezys/home_screens/main_screen.dart';
 import 'package:ezys/home_screens/product_detail_screen.dart';
+import 'package:ezys/model/product.dart';
+import 'package:ezys/services/api_service.dart';
+import 'package:ezys/services/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class WishList extends StatefulWidget {
   const WishList({super.key});
@@ -16,6 +22,14 @@ class _WishListState extends State<WishList> {
   bool isLiked = false;
   int counter = 0;
   List<String> subcategory = [];
+  Future<void>? listItems;
+  List<Product> products = [];
+
+  @override
+  void initState() {
+    listItems = getItems();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,181 +43,184 @@ class _WishListState extends State<WishList> {
             color: Colors.black,
           ),
         ),
-        mainChild: CustomScrollView(
-          slivers: [
-            SliverPersistentHeader(
-              floating: true,
-              pinned: true,
-              delegate: PersistentHeader(
-                widget: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  child: SizedBox(
-                      width: double.infinity,
-                      child: ListView.builder(
-                          scrollDirection: Axis.horizontal,
-                          itemCount: 1,
-                          itemBuilder: (context, index) {
-                            return MultiSelectChoiceChips(
-                              options: const [
-                                'All',
-                                'Latest',
-                                'Hot Deals',
-                                'Men',
-                                'Women',
-                                'Boys',
-                                'Girls',
-                              ],
-                              onSelectionChanged: (selectedOptions) {
-                                setState(() {
-                                  subcategory = selectedOptions;
-                                });
-                              },
-                            );
-                          })),
-                ),
-              ),
-            ),
-            SliverPadding(
-              padding: const EdgeInsets.all(10.0),
-              sliver: SliverGrid.builder(
-                  itemCount: 8,
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    mainAxisSpacing: 15,
-                    crossAxisSpacing: 15,
-                    crossAxisCount: 2,
-                    mainAxisExtent: 241,
-                  ),
-                  itemBuilder: ((context, index) {
-                    return Container(
-                      padding: const EdgeInsets.all(5),
-                      decoration: BoxDecoration(
-                        border: Border.all(width: 0.25, color: Colors.grey),
-                        borderRadius: BorderRadius.circular(12),
-                        color: Colors.white,
-                        // image: DecorationImage(image: AssetImage('assets/image.jpeg'))
-                      ),
-                      child: Stack(
-                        children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              GestureDetector(
-                                  onTap: () {
-                                    Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (context) =>
-                                                const ProductDetail(
-                                                    itemPrice: "",
-                                                    productId: "")));
-                                  },
-                                  child: ClipRRect(
-                                    borderRadius: BorderRadius.circular(12),
-                                    child: Container(
-                                      height: 150,
-                                      width: double.infinity,
-                                      decoration: BoxDecoration(
-                                        borderRadius: BorderRadius.circular(12),
-                                      ),
-                                      child: Stack(
-                                        children: [
-                                          Image.asset(
-                                            'assets/image.jpeg',
-                                            fit: BoxFit.cover,
-                                            height: 150,
-                                            width: double.infinity,
-                                          ),
-                                          Container(
-                                            decoration: BoxDecoration(
-                                              borderRadius:
-                                                  BorderRadius.circular(12),
-                                              gradient: const LinearGradient(
-                                                // stops: [0,5],
-                                                begin: Alignment.topRight,
-                                                end: Alignment.bottomLeft,
-                                                colors: [
-                                                  Colors.black26,
-                                                  Colors.transparent
-                                                ],
-                                              ),
-                                            ),
-                                          ),
-                                        ],
-                                      ),
-                                    ),
-                                  )),
-                              const SizedBox(
-                                height: 5,
-                              ),
-                              const Text('T-shirt',
-                                  style: TextStyle(color: Colors.grey)),
-                              const SizedBox(height: 5),
-                              const Text("Tiger Image EZYE – Tees for Men"),
-                              const SizedBox(height: 5),
-                              Row(
-                                children: [
-                                  Text(
-                                    '₹799',
-                                    style: TextStyle(
-                                        decoration: TextDecoration.lineThrough,
-                                        color: Colors.green[800],
-                                        decorationColor: Colors.green[800],
-                                        decorationThickness: 3),
-                                  ),
-                                  const SizedBox(width: 5),
-                                  const Text('- ₹399'),
-                                ],
-                              )
-                            ],
-                          ),
-                          Positioned(
-                              top: 0,
-                              right: 0,
-                              child: Column(
-                                children: [
-                                  Container(
-                                    padding: const EdgeInsets.all(5),
-                                    decoration: const BoxDecoration(
-                                      shape: BoxShape.circle,
-                                    ),
-                                    child: GestureDetector(
-                                      onTap: () {
+        mainChild: FutureBuilder<void>(
+            future: listItems,
+            builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                if (products.isEmpty) {
+                  return const Center(child: Text("No items found!"));
+                }
+
+                return CustomScrollView(
+                  slivers: [
+                    SliverPersistentHeader(
+                      floating: true,
+                      pinned: true,
+                      delegate: PersistentHeader(
+                        widget: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10),
+                          child: SizedBox(
+                              width: double.infinity,
+                              child: ListView.builder(
+                                  scrollDirection: Axis.horizontal,
+                                  itemCount: 1,
+                                  itemBuilder: (context, index) {
+                                    return MultiSelectChoiceChips(
+                                      options: const [
+                                        'All',
+                                        'Latest',
+                                        'Hot Deals',
+                                        'Men',
+                                        'Women',
+                                        'Boys',
+                                        'Girls',
+                                      ],
+                                      onSelectionChanged: (selectedOptions) {
                                         setState(() {
-                                          isLiked = !isLiked;
+                                          subcategory = selectedOptions;
                                         });
                                       },
-                                      child: Image.asset(
-                                        isLiked
-                                            ? 'assets/icons/Comp 232.gif'
-                                            : 'assets/icons/White heart.png',
-                                        height: 30,
-                                        width: 30,
-                                      ),
-                                    ),
-                                  ),
-                                  const SizedBox(height: 10),
-                                  // Container(
-                                  //   padding: EdgeInsets.all(5),
-                                  //   decoration: BoxDecoration(
-                                  //       shape: BoxShape.circle,
-                                  //       color: Colors.white),
-                                  //   child: GestureDetector(
-                                  //     onTap: () {},
-                                  //     child: Image.asset(
-                                  //       'assets/icons/shoppingbag.png',
-                                  //       height: 17,
-                                  //       width: 17,
-                                  //     ),
-                                  //   ),
-                                  // ),
-                                ],
-                              )),
-                        ],
+                                    );
+                                  })),
+                        ),
                       ),
-                    );
-                  })),
-            ),
-          ],
-        ));
+                    ),
+                    SliverPadding(
+                      padding: const EdgeInsets.all(10.0),
+                      sliver: SliverGrid.builder(
+                          itemCount: products.length,
+                          gridDelegate:
+                              const SliverGridDelegateWithFixedCrossAxisCount(
+                            mainAxisSpacing: 15,
+                            crossAxisSpacing: 15,
+                            crossAxisCount: 2,
+                            mainAxisExtent: 241,
+                          ),
+                          itemBuilder: ((context, index) {
+                            Product product = products[index];
+
+                            return Container(
+                              padding: const EdgeInsets.all(5),
+                              decoration: BoxDecoration(
+                                border:
+                                    Border.all(width: 0.25, color: Colors.grey),
+                                borderRadius: BorderRadius.circular(12),
+                                color: Colors.white,
+                                // image: DecorationImage(image: AssetImage('assets/image.jpeg'))
+                              ),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  GestureDetector(
+                                      onTap: () {
+                                        Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    ProductDetail(
+                                                        itemPrice:
+                                                            product.sellingPrice ??
+                                                                '',
+                                                        productId:
+                                                            product.productId ??
+                                                                '')));
+                                      },
+                                      child: ClipRRect(
+                                        borderRadius: BorderRadius.circular(12),
+                                        child: Container(
+                                          height: 150,
+                                          width: double.infinity,
+                                          decoration: BoxDecoration(
+                                            borderRadius:
+                                                BorderRadius.circular(12),
+                                          ),
+                                          child: Stack(
+                                            children: [
+                                              Image.asset(
+                                                'assets/image.jpeg',
+                                                fit: BoxFit.cover,
+                                                height: 150,
+                                                width: double.infinity,
+                                              ),
+                                              Container(
+                                                decoration: BoxDecoration(
+                                                  borderRadius:
+                                                      BorderRadius.circular(12),
+                                                  gradient:
+                                                      const LinearGradient(
+                                                    // stops: [0,5],
+                                                    begin: Alignment.topRight,
+                                                    end: Alignment.bottomLeft,
+                                                    colors: [
+                                                      Colors.black26,
+                                                      Colors.transparent
+                                                    ],
+                                                  ),
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      )),
+                                  const SizedBox(
+                                    height: 5,
+                                  ),
+                                  Text('${product.name}',
+                                      style: TextStyle(color: Colors.grey)),
+                                  const SizedBox(height: 5),
+                                  Text('${product.description}'),
+                                  const SizedBox(height: 5),
+                                  Row(
+                                    children: [
+                                      Text(
+                                        '₹${product.mrp}',
+                                        style: TextStyle(
+                                            decoration:
+                                                TextDecoration.lineThrough,
+                                            color: Colors.green[800],
+                                            decorationColor: Colors.green[800],
+                                            decorationThickness: 3),
+                                      ),
+                                      const SizedBox(width: 5),
+                                      Text('- ₹${product.sellingPrice}'),
+                                    ],
+                                  )
+                                ],
+                              ),
+                            );
+                          })),
+                    ),
+                  ],
+                );
+              }
+
+              if (snapshot.hasError) {
+                return const Text(
+                    "Something went wrong. Please try again later");
+              }
+
+              return Center(
+                child: Image.asset(
+                  'assets/animation.gif',
+                  height: 150,
+                  width: 250,
+                ),
+              );
+            }));
+  }
+
+  Future<void> getItems() async {
+    try {
+      var response = await http.post(
+          Uri.parse('${ApiService.url}/getUserWishlist.php'),
+          body: {"userId": FirebaseUser.user?.uid ?? ''});
+      products = (json.decode(response.body) as List)
+          .map((item) => Product.fromJson(item))
+          .toList();
+
+      print(response.body);
+    } catch (e) {
+      debugPrint(e.toString());
+    }
   }
 }
