80d547eafd91468b1d5c61b78b4053fa6ade2d92
digitalspike <digitalspikeapps@gmail.com>
2024-01-27 06:47:20 +0530

    Variant added

--- lib/model/product.dart
+++ lib/model/product.dart
@@ -18,6 +18,7 @@ class Product {
   String? stockStatus;
   String? stockNumber;
   bool isSaved;
+  List<ProductVariant>? variant;
 
   Product(
       {this.id,
@@ -38,28 +39,70 @@ class Product {
       this.size,
       this.stockNumber,
       this.stockStatus,
-      this.isSaved = false});
+      this.isSaved = false,
+      this.variant});
 
   factory Product.fromJson(Map<String, dynamic> json) {
     return Product(
+        id: json['id'],
+        name: json['name'],
+        category: json['category'],
+        description: json['description'],
+        fullDescription: json['fullDescription'],
+        image1Url: json['image1Url'],
+        image2Url: json['image2Url'],
+        image3Url: json['image3Url'],
+        image4Url: json['image4Url'],
+        image5url: json['image5url'],
+        mrp: json['MRP'],
+        productId: json['productId'],
+        sellingPrice: json['sellingPrice'],
+        subCategory: json['subCategory'],
+        color: json['color'],
+        size: json['size'],
+        stockNumber: json['stock_status'],
+        stockStatus: json['stock_number'],
+        variant: json['varientDetails'] != null
+            ? (json['varientDetails'] as List)
+                .map((variant) => ProductVariant.fromJson(variant))
+                .toList()
+            : null);
+  }
+}
+
+class ProductVariant {
+  String? id;
+  String? productId;
+  String? size;
+  String? color;
+  String? amount;
+  String? stockStatus;
+  String? stockNumber;
+  String? variantId;
+  String? image;
+
+  ProductVariant(
+      {this.size,
+      this.id,
+      this.amount,
+      this.color,
+      this.image,
+      this.productId,
+      this.stockNumber,
+      this.stockStatus,
+      this.variantId});
+
+  factory ProductVariant.fromJson(Map<String, dynamic> json) {
+    return ProductVariant(
       id: json['id'],
-      name: json['name'],
-      category: json['category'],
-      description: json['description'],
-      fullDescription: json['fullDescription'],
-      image1Url: json['image1Url'],
-      image2Url: json['image2Url'],
-      image3Url: json['image3Url'],
-      image4Url: json['image4Url'],
-      image5url: json['image5url'],
-      mrp: json['MRP'],
       productId: json['productId'],
-      sellingPrice: json['sellingPrice'],
-      subCategory: json['subCategory'],
-      color: json['color'],
       size: json['size'],
-      stockNumber: json['stock_status'],
-      stockStatus: json['stock_number'],
+      color: json['color'],
+      amount: json['amount'],
+      stockStatus: json['stock_status'],
+      stockNumber: json['stock_number'],
+      variantId: json['varientId'],
+      image: json['image'],
     );
   }
 }
